7d7ac80187db39f844ab11ad4dc45c17e69b0038
thibault173 <thibault.villard@gmail.com>
2021-06-03 21:11:13 +0200

    WIP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
+
+config.action_dispatch.show_exceptions = true
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
